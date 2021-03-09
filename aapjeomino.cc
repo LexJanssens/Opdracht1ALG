5ee2008c4425e3e8021c46183fc71de923d6b2ba
Bart-Aaldering <79908741+Bart-Aaldering@users.noreply.github.com>
2021-03-09 14:55:25 +0100

    Update aapjeomino.cc

--- aapjeomino.cc
+++ aapjeomino.cc
@@ -9,96 +9,105 @@
 
 AapjeOmino::AapjeOmino ()
 {
-  // TODO: implementeer (zo nodig) deze constructor
+// TODO: implementeer (zo nodig) deze constructor
 
 }  // default constructor
 
 //*************************************************************************
 
-bool AapjeOmino::leesIn (const char* invoernaam){
-	// TODO: implementeer deze memberfunctie
-	int beginStenen, rijStartSteen, kolomStartSteen, getal, steen[4];
+bool AapjeOmino::leesIn (const char* invoernaam) 
+{
+   // TODO: implementeer deze memberfunctie
+   int beginStenen, rijStartSteen, kolomStartSteen, getal;
 
-	ifstream invoer (invoernaam, ios::in);
+   ifstream invoer (invoernaam, ios::in);
 
-	if (!invoer.is_open()) { // checkt of het bestand wel to openen is
-      cout << "Kan file niet openen." << endl;
+   if (!invoer.is_open()) { // checkt of het bestand wel to openen is
+         cout << "Kan file niet openen." << endl;
+         return false;
+   }
+
+   invoer >> hoogte;
+   invoer.get();
+   if (hoogte > MaxDimensie){
+      cout << "Het bord is te hoog." << endl;
       return false;
-	}
+   }
 
-	invoer >> hoogte;
-	invoer.get();
-	if (hoogte > MaxDimensie){
-		cout << "Het bord is te hoog." << endl;
-		return false;
-	}
+   invoer >> breedte;
+   invoer.get();
+   if (breedte > MaxDimensie){
+      cout << "Het bord is te breedt." << endl;
+      return false;
+   }
+   
+   invoer >> nrStenen;
+   invoer.get();
+   invoer >> beginStenen;
+   invoer.get();
+   if (nrStenen < beginStenen * 2 + 1) {
+      cout << "Er zijn te weinig stenen." << endl;
+      return false;
+   }
 
-	invoer >> breedte;
-	invoer.get();
-	if (breedte > MaxDimensie){
-		cout << "Het bord is te breedt." << endl;
-		return false;
-	}
-
-	invoer >> nrStenen;
-	invoer.get();
-	invoer >> beginStenen;
-	invoer.get();
-	if (nrStenen < beginStenen * 2 + 1) {
-		cout << "Er zijn te weinig stenen." << endl;
-		return false;
-	}
-
-	invoer >> rijStartSteen;
-	invoer.get();
-	invoer >> kolomStartSteen;
-	invoer.get();
-	if (rijStartSteen >= MaxDimensie || kolomStartSteen >= MaxDimensie || rijStartSteen < 0 || kolomStartSteen < 0) {
-		cout << "De startsteen ligt niet op het bord." << endl;
-		return false;
-	}
-
-	for (int i = 0; i < nrStenen; i++) {
-      pot[i] = i;
-		for (int j = 0; j < 4; j++) {
-			invoer >> getal;
-			stenen[i][j] = getal;
-		}
-		invoer.get();
-	}
-	//Bord op -1, zodat er geen stenen zijn.
+   invoer >> rijStartSteen;
+   invoer.get();
+   invoer >> kolomStartSteen;
+   invoer.get();
+   if (rijStartSteen >= MaxDimensie || kolomStartSteen >= MaxDimensie || rijStartSteen < 0 || kolomStartSteen < 0) {
+      cout << "De startsteen ligt niet op het bord." << endl;
+      return false;
+   }
+   
+   //Bord op -1, zodat er geen stenen zijn.
    for (int i = 0; i < MaxDimensie; i++) {
       for (int j = 0; j < MaxDimensie; j++) {
          bord[i][j].first = -1;
          bord[i][j].first = -1;
       }
    }
-	bord[rijStartSteen][kolomStartSteen].first = 0; //steennr
-	bord[rijStartSteen][kolomStartSteen].second = 0; //rotatie
 
-	invoer.close();
-	for (int i = 0; i < beginStenen; i++) {
-		haalSteenUitPot();
-		wisselSpeler();
-		haalSteenUitPot();
-		wisselSpeler();
-	}
-	return true;
-}  // leesIn
+   // stopt de stenen in de index lijst
+   // en initialiseert 
+   for (int i = 0; i < nrStenen; i++) { 
+      stenenFemke[i] = -1;
+      stenenLieke[i] = -1;
+      for (int j = 0; j < 4; j++) {
+         invoer >> getal;
+         stenen[i][j] = getal;
+      }
+      invoer.get();
+   }
+
+   // legen beginsteen
+   bord[rijStartSteen][kolomStartSteen].first = 0;
+   bord[rijStartSteen][kolomStartSteen].second = 0;
+
+   //verdelen van de beginstenen aan de spelers
+   for (int i = 0; i < beginStenen; i++) {
+      haalSteenUitPot();
+      wisselSpeler();
+      haalSteenUitPot();
+      wisselSpeler();
+   }
+
+   invoer.close();
+   
+   return true;
+}	// leesIn
 
 //*************************************************************************
 
 bool AapjeOmino::eindstand ()
 {
-  // TODO: implementeer deze memberfunctie
-  //Hier komt de eindstand functie te staan
-  return false;
+// TODO: implementeer deze memberfunctie
+
+return false;
 
 }  // eindstand
 
 //*************************************************************************
 
-//n en m worden lengte en hoogte. N O Z W kunnen worden aangepast naar bord[i][j] uiteindelijk
 void AapjeOmino::drukAf()
 {
    for (int i = 0; i < breedte; i++) {
@@ -146,9 +155,9 @@ void AapjeOmino::drukAf()
 vector<Zet> AapjeOmino::bepaalMogelijkeZetten ()
 { vector<Zet> zetten;
 
-  // TODO: implementeer deze memberfunctie
+// TODO: implementeer deze memberfunctie
 
-  return zetten;
+return zetten;
 
 }  // bepaalMogelijkeZetten
 
@@ -170,24 +179,23 @@ int AapjeOmino::haalSteenUitPot ()
       stenenLieke[i] = pot;
       pot++;
    }
-  return 0;
-
+   return 0;
 }  // haalSteenUitPot
 
 //*************************************************************************
 
 void AapjeOmino::wisselSpeler ()
 {
-  aanBeurt = 1-aanBeurt;
+   aanBeurt = 1 - aanBeurt;
 }  // wisselSpeler
 
 //*************************************************************************
 
 bool AapjeOmino::doeZet (Zet zet)
 {
-  // TODO: implementeer deze memberfunctie
+// TODO: implementeer deze memberfunctie
 
-  return true;
+return true;
 
 }  // doeZet
 
@@ -196,31 +204,30 @@ bool AapjeOmino::doeZet (Zet zet)
 vector<Zet> AapjeOmino::bepaalGoedeZetten ()
 { vector<Zet> zetten;
 
-  // TODO: implementeer deze memberfunctie
+// TODO: implementeer deze memberfunctie
 
-  return zetten;
-
+   return zetten;
 }  // bepaalGoedeZetten
 
 //*************************************************************************
 
 int AapjeOmino::besteScore (Zet &besteZet, long long &aantalStanden)
 {
-  // TODO: implementeer deze memberfunctie
+// TODO: implementeer deze memberfunctie
 
-  return 0;
+return 0;
 
 }  // besteScore
 
 //*************************************************************************
 
 bool AapjeOmino::genereerRandomSpel (int hoogte0, int breedte0,
-       int nrStenen0, int nrStenenInHand0, int rij0, int kolom0,
-       int minGetal, int maxGetal)
+      int nrStenen0, int nrStenenInHand0, int rij0, int kolom0,
+      int minGetal, int maxGetal)
 {
-  // TODO: implementeer deze memberfunctie
+// TODO: implementeer deze memberfunctie
 
-  return true;
+return true;
 
 }  // genereerRandomSpel
 
