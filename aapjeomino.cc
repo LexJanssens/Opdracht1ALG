66b6bd28f7cddbe28eacac54290ac0e82975884d
Bart-Aaldering <79908741+Bart-Aaldering@users.noreply.github.com>
2021-03-26 22:57:15 +0100

    Update aapjeomino.cc

--- aapjeomino.cc
+++ aapjeomino.cc
@@ -4,12 +4,40 @@
 #include "standaard.h"
 #include "aapjeomino.h"
 #include "fstream"
+#include <algorithm>
 
 //*************************************************************************
 
 AapjeOmino::AapjeOmino()
 {
 }  // default constructor
+
+//*************************************************************************
+
+// returnt wie er aan de beurt is
+int AapjeOmino::fAanBeurt()
+{
+	return aanBeurt;
+}
+
+//*************************************************************************
+
+// sorteert de vector A
+vector<int> AapjeOmino::sorteer(vector<int> &A)
+{
+	int n = A.size();
+	int temp;
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < n -i; j++) {
+			if (A[j] > A[j+1]) {
+				temp = A[j]; 
+				A[j] = A[j+1]; 
+				A[j+1] = temp;
+			}
+		}
+	}
+	return A;
+}
 
 //*************************************************************************
 
@@ -178,7 +206,7 @@ void AapjeOmino::drukAf()
 	}
 	cout << endl;
 	if (aanBeurt)
-		cout << "Lieke iaantalMogenlijkeZetten aan de beurt" << endl;
+		cout << "Lieke is aan de beurt" << endl;
 	else
 		cout << "Femke is aan de beurt" << endl;
 	if (eindstand()) {
@@ -451,11 +479,11 @@ vector<Zet> AapjeOmino::bepaalGoedeZetten()
 // En stopt deze terug in de pot
 void AapjeOmino::doeSteenInPot()
 {
-   pot--;
-   if (!aanBeurt)
-      stenenFemke.pop_back();
-   else
-      stenenLieke.pop_back();
+	pot--;
+	if (!aanBeurt)
+		stenenFemke.pop_back();
+	else
+		stenenLieke.pop_back();
 }  // doeSteenInPot
 
 //*************************************************************************
@@ -471,9 +499,13 @@ void AapjeOmino::unDoeZet (Zet zet)
 
 	if (!aanBeurt) {
 		stenenFemke.push_back(zet.getI());
+		sort(stenenFemke.begin(), stenenFemke.end());
+		// sorteer(stenenFemke);
 	}
 	else {
 		stenenLieke.push_back(zet.getI());
+		sort(stenenLieke.begin(), stenenLieke.end());
+		// sorteer(stenenLieke);
 	}
 }  // unDoeZet
 
@@ -514,7 +546,7 @@ int AapjeOmino::besteScore (Zet &besteZet, long long &aantalStanden)
 			haalSteenUitPot();
 			zetten = bepaalMogelijkeZetten();
 			aantalMogenlijkeZetten = zetten.size();
-			if (aantalMogenlijkeZetten == 0) { // na pakken nog steeds geen zet
+			if (aantalMogenlijkeZetten == 0) { // na pakken kan nog steeds geen zet
 				wisselSpeler();
 				score = -besteScore(besteZet, aantalStanden);
 				if (score > maxscore) {
@@ -522,11 +554,11 @@ int AapjeOmino::besteScore (Zet &besteZet, long long &aantalStanden)
 					besteZet.setDefaultWaardes();
 				}
 				wisselSpeler();
-			} else { // na pakken kan er wel een zet
+			} else { // na het pakken kan er wel een zet
 				for (int i = 0; i < aantalMogenlijkeZetten; i++) {
-					doeZet(zetten[i]);
+					doeZet(zetten[i]);	
 					score = -besteScore(besteZet, aantalStanden);
-					if (score > maxscore) {
+					if (score >= maxscore) {
 						maxscore = score;
 						besteZet = zetten[i];
 					}
@@ -559,7 +591,8 @@ bool AapjeOmino::genereerRandomSpel (
 	int nrStenen0, int nrStenenInHand0, int rij0, int kolom0,
 	int minGetal, int maxGetal)
 {
-   if (!integerInBereik ("hoogte0", hoogte0, 1, MaxDimensie))
+
+	if (!integerInBereik ("hoogte0", hoogte0, 1, MaxDimensie))
 		return false;
 
 	if (!integerInBereik ("breedte0", breedte0, 1, MaxDimensie))
@@ -570,29 +603,25 @@ bool AapjeOmino::genereerRandomSpel (
 		return false;
 	}
 
-	if (!integerInBereik("rij0", rij0, 1, breedte0) ||
-      !integerInBereik("kolom0", kolom0, 1, hoogte0)) {
-		cout << "De startsteen ligt niet op het bord." << endl;
+	if (!integerInBereik("rij0", rij0, 1, hoogte0) ||
+		!integerInBereik("kolom0", kolom0, 1, breedte0))
 		return false;
-	}
-   hoogte = hoogte0;
-   breedte = breedte0;
-   nrStenen = nrStenen0;
-	beginStenen = nrStenenInHand0;
 
-	//Bord op -1 & 0, zodat er geen stenen zijn.
+	// Bord op -1 & 0, zodat het bord leeg is.
 	for (int i = 0; i < MaxDimensie; i++) {
 		for (int j = 0; j < MaxDimensie; j++) {
 			bord[i][j].first = -1;
 			bord[i][j].second = 0;
 		}
 	}
-   //genereer stenen
-   for (int i = 0; i < nrStenen0; i++) {
+
+	// genereer de stenen
+	for (int i = 0; i < nrStenen0; i++) {
 		for (int j = 0; j < 4; j++) {
 			stenen[i][j] = randomGetal(minGetal, maxGetal);
 		}
-   }
+	}
+
 	// leggen beginsteen
 	bord[rij0][kolom0].first = 0;
 	bord[rij0][kolom0].second = 0;
@@ -600,11 +629,11 @@ bool AapjeOmino::genereerRandomSpel (
 	//verdelen van de beginstenen aan de spelers
 	for (int i = 0; i < nrStenenInHand0 * 2; i++) {
 		if (aanBeurt)
-         stenenLieke.push_back(pot);
-      else
-         stenenFemke.push_back(pot);
-      pot++;
-      wisselSpeler();
+			stenenLieke.push_back(pot);
+		else
+			stenenFemke.push_back(pot);
+		pot++;
+		wisselSpeler();
 	}
 	return true;
 }  // genereerRandomSpel
