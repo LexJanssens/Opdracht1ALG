0bb28a548046579cbacd4c11a7cb14445643bee5
Bart-Aaldering <79908741+Bart-Aaldering@users.noreply.github.com>
2021-03-27 14:17:13 +0100

    Update [KLAAR] zet.h

--- [KLAAR] zet.h
+++ [KLAAR] zet.h
@@ -4,32 +4,32 @@
 #define ZetHVar  // ge-include wordt
 
 class Zet
-{ public:
+{ 
+	public:
+	// Default constructor
+	Zet ();
 
-    // Default constructor
-    Zet ();
+	// Geef de vier velden waardes die in combinatie niet voor kunnen
+	// komen, als default voor `geen echte zet'.
+	void setDefaultWaardes ();
 
-    // Geef de vier velden waardes die in combinatie niet voor kunnen
-    // komen, als default voor `geen echte zet'.
-    void setDefaultWaardes ();
-    
-    // Geef alle vier velden een waarde.
-    // Controleer nog wel of het zinnige waardes zijn.
-    void setWaardes (int i0, int r0, int rij0, int kolom0);
+	// Geef alle vier velden een waarde.
+	// Controleer nog wel of het zinnige waardes zijn.
+	void setWaardes (int i0, int r0, int rij0, int kolom0);
 
-    // Vier getters:
-    int getI ();
-    int getR ();
-    int getRij ();
-    int getKolom ();
+	// Vier getters:
+	int getI ();
+	int getR ();
+	int getRij ();
+	int getKolom ();
 
-    // Druk de vier waardes van de zet af op het scherm
-    void drukAf ();
+	// Druk de vier waardes van de zet af op het scherm
+	void drukAf ();
 
-  private:
-    int i,  // nummer van de steen
-        r,  // rotatie
-        rij, kolom;  // vakje op het bord
+	private:
+	int i,  // nummer van de steen
+		r,  // rotatie
+		rij, kolom;  // vakje op het bord
 
 };
 
